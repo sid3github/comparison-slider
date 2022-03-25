1963f83f1e2bc66ec36021504c37752bd2f8b116
unknown <siddharthpadwal3@gmail.com>
2022-03-25 11:48:39 +0530

    created comparison slider

---
+++ library.js
@@ -0,0 +1,23 @@
+// function $(selector) {
+// 	const self = {
+// 		element: document.querySelector(selector),
+// 		val: () => self.element,
+// 		css: () => {
+// 			self.element.style.clipPath =
+// 				"polygon(0 0," + this.val + "% 0," + this.val + "% 100%, 0 100%)";
+// 		},
+// 		on: (event, callback) => {
+// 			document.addEventListener(event, callback);
+// 		},
+// 	};
+// 	return self;
+// }
+
+// let slide = $("#slider");
+
+// let slideValue;
+// let img;
+
+// $(slide).on("input", function () {
+// 	$("#my-img").css();
+// });
